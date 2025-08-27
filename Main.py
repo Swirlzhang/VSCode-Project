56c252bff197e06a93293ab43460294ce509d698
Swirlzhang <swirlzhang@gmail.com>
2025-08-27 21:06:20 +0800

    Add days_between_dates function to calculate the number of days between two dates and update print statement

--- Main.py
+++ Main.py
@@ -5,6 +5,29 @@ from Package_B.Package_B_2 import Module_B_2
 
 print(Module_A_1.A_1)
 # function that calculates how many days between two dates 
+def days_between_dates(date1, date2):
+    from datetime import datetime
+    d1 = datetime.strptime(date1, "%Y-%m-%d")
+    d2 = datetime.strptime(date2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
+
+print(days_between_dates("2023-01-01", "2023-12-31"))
+
+# function that connects to mongo db
+def connect_to_mongo(db_name):
+    from pymongo import MongoClient
+    client = MongoClient('localhost', 27017)
+    db = client[db_name]
+    return db
+
+print(__name__)
 
 
+list_A: Any = [1, 2, None]
+if all(list_A):
+    print("All elements are truthy" )
+elif any(list_A):
+    print("At least one element is truthy" )
+else:
+    print("No elements are truthy" )    
 
